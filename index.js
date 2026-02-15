921bb15125fb00f677fd7683f91c94907145105a
Lincoln <lincolnbernard7@gmail.com>
2026-02-15 18:29:05 -0500

    Added touch action CSS to disable IOS scroll bug

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ function isNewGame () {
 
 function onDragStart (source, piece, position, orientation) {
   //Fix for drag and drop conflicting with page scroll on mobile
-  document.getElementsByTagName('body')[0].style.touchaction = "auto";
+  document.getElementsByTagName('body')[0].css('touch-action', 'none')
 
   //Alert UCI user is starting the game
   if(isNewGame()) {
@@ -216,7 +216,7 @@ function onSnapEnd () {
   board.position(game.fen())
 
   //Fix for drag and drop conflicting with page scroll on mobile
-  document.getElementsByTagName('body')[0].style.touchaction = "auto";
+  document.getElementsByTagName('body')[0].css('touch-action', 'auto')
 }
 
 function updateStatus () {
